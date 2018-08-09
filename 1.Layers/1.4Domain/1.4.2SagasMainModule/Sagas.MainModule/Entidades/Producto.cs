c8bbe476cf359d1eaa4484a78777a9d610b4e8e8
Kevin Salomon <kevin.salomon@neoteck.com.mx>
2018-08-09 16:21:00 -0500

    KSH-09/08/2018 Agregado de los campos EsActivoVenta y EsGAs de Producto
    Se agregaron los campos de EsActivoVenta y EsGas en la entidad Producto.

--- 1.Layers/1.4Domain/1.4.2SagasMainModule/Sagas.MainModule/Entidades/Producto.cs
+++ 1.Layers/1.4Domain/1.4.2SagasMainModule/Sagas.MainModule/Entidades/Producto.cs
@@ -34,6 +34,8 @@ namespace Sagas.MainModule.Entidades
         public short IdUnidadMedida { get; set; }
         public Nullable<short> IdUnidadMedida2 { get; set; }
         public string Descripcion { get; set; }
+        public bool EsActivoVenta { get; set; }
+        public bool EsGas { get; set; }
         public Nullable<decimal> Minimos { get; set; }
         public Nullable<decimal> Maximo { get; set; }
         public string UrlImagen { get; set; }
