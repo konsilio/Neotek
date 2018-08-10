3b38171a18a9d597152d6f3044e6fe4c6854f981
Celina Villicaña <celina.villicana@neoteck.com.mx>
2018-08-10 08:51:24 -0500

    CVG - 10/08/2018 Web appi orden compra
    integración de los cambios de orden de compra en data access

--- 1.Layers/1.3Application/1.3.2MainModule/Application.MainModule/Servicios/AccesoADatos/OrdenCompraDataAccess.cs
+++ 1.Layers/1.3Application/1.3.2MainModule/Application.MainModule/Servicios/AccesoADatos/OrdenCompraDataAccess.cs
@@ -23,10 +23,11 @@ namespace Application.MainModule.Servicios.AccesoADatos
             var productos = uow.Repository<OrdenCompraProducto>().Get(x => x.EsGas && x.EsActivoVenta);
 
 
-             productos = uow.Repository<OrdenCompraProducto>().Get(x => x.EsGas && x.EsActivoVenta 
-                                                                     && x.OrdenCompra.Activo
-                                                                     && x.OrdenCompra.IdEmpresa.Equals(idEmpresa)
-                                                                     && x.OrdenCompra.OrdenCompraEstatus.Equals(idOrdenComprEstatus));
+            //productos = uow.Repository<OrdenCompraProducto>().Get(x => x.EsGas && x.EsActivoVenta 
+            //                                                        && x.OrdenCompra.Activo
+            //                                                        && x.OrdenCompra.IdEmpresa.Equals(idEmpresa)
+            //                                                        && x.OrdenCompra.OrdenCompraEstatus.Equals(idOrdenComprEstatus));
+
             List<OrdenCompra> ordencompra = new List<OrdenCompra>();
             foreach (OrdenCompraProducto o in productos)
             {
