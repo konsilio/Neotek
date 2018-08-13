53b39ec6c95b255ec581cbd7477bbe5f302985ef
Kevin Salomon <kevin.salomon@neoteck.com.mx>
2018-08-13 18:14:32 -0500

    KSH-13/08/2018 Se agregó el proyecto para enviar correos electrónicos
    Se agregó al Cross Cutting el proyecto que envía correos electrónicos.

---
+++ 1.Layers/1.5Infrastructure/1.5.2Cross Cutting/1.5.2.3Mail/IMensajeCorreo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Mail.MainModule
+{
+    interface IMensajeCorreo
+    {
+        string Emisor { set; }
+        IEnumerable<string> Destinatarios { set; }
+        string Mensaje { set; }
+        string Asunto { set; }
+        void Enviar();
+    }
+}
