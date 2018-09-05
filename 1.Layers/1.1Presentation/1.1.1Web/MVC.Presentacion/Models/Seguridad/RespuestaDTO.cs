78f0f86a0a692b3e2d9f8e5b6599016251b47505
Jaime Santillan <jaime.santillan@neoteck.com.mx>
2018-09-05 13:51:09 -0500

    JSA-Avance en Login MVC

---
+++ 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Models/Seguridad/RespuestaDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Presentacion.Models.Seguridad
+{
+    public class RespuestaDTO
+    {
+        public bool Exito { get; set; }
+        public bool EsInsercion { get; set; }
+        public bool EsActulizacion { get; set; }
+        public string Mensaje { get; set; }
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public bool ModeloValido { get; set; }
+        public List<string> MensajesError { get; set; }
+        public string RedirigirUrl { get; set; }
+    }
+}
