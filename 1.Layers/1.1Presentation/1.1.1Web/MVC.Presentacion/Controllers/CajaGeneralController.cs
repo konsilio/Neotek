56639c56f0d148d33f7c2d3d3bd645f81abb1ce9
glcjal <celia.ambriz@neoteck.com.mx>
2018-10-03 18:37:21 -0500

    CJAL-03/10/2018-Avance Vistas Caja General

---
+++ 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Controllers/CajaGeneralController.cs
@@ -0,0 +1,57 @@
+﻿using MVC.Presentacion.App_Code;
+using MVC.Presentacion.Models.Ventas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Presentacion.Controllers
+{
+    public class CajaGeneralController : Controller
+    {
+        // GET: CajaGeneral
+        public ActionResult Index()
+        {
+            if (Session["StringToken"] == null) return RedirectToAction("Index", "Home", AutenticacionServicio.InitIndex(new Models.Seguridad.LoginModel()));
+            string _tkn = Session["StringToken"].ToString();
+
+            ViewBag.EsSuperUser = TokenServicio.ObtenerEsSuperUsuario(_tkn);
+
+            if (ViewBag.EsSuperUser)
+            {
+                ViewBag.Empresas = CatalogoServicio.Empresas(_tkn);
+                ViewBag.ListaPV = CatalogoServicio.ListaPrecioVenta(0, _tkn);
+            }
+            else
+            {
+                ViewBag.Empresas = CatalogoServicio.Empresas(_tkn).SingleOrDefault(x => x.IdEmpresa.Equals(TokenServicio.ObtenerIdEmpresa(_tkn))).NombreComercial;
+                ViewBag.ListaPV = CatalogoServicio.ListaPrecioVentaIdEmpresa(TokenServicio.ObtenerIdEmpresa(_tkn), _tkn);
+            }
+
+            ViewBag.ListaEntidad = CatalogoServicio.ListaTipoFecha(_tkn);
+            ViewBag.ListaConcepto = CatalogoServicio.ListaTipoFecha(_tkn);
+            if (TempData["RespuestaDTO"] != null)
+            {
+                ViewBag.MessageExito = TempData["RespuestaDTO"];
+            }
+            if (TempData["RespuestaDTOError"] != null)
+            {
+                ViewBag.MessageError = TempData["RespuestaDTOError"];
+            }
+
+            ViewBag.MessageError = TempData["RespuestaDTOError"];
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Liquidar(CajaGeneralModel _ObjModel)
+        {
+            if (Session["StringToken"] == null) return RedirectToAction("Index", "Home", AutenticacionServicio.InitIndex(new Models.Seguridad.LoginModel()));
+            string _tok = Session["StringToken"].ToString();
+
+            return View();
+        }
+    }
+}
