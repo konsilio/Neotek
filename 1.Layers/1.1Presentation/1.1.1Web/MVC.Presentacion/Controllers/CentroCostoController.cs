cf472e462a182a758be3aced25ba10c4659eff7c
Jaime Santillan <jaime.santillan@neoteck.com.mx>
2018-10-12 17:52:04 -0500

    JSA-12/10/2018-Correcciones de flujo

--- 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Controllers/CentroCostoController.cs
+++ 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Controllers/CentroCostoController.cs
@@ -1,6 +1,7 @@
 ﻿using MVC.Presentacion.App_Code;
 using MVC.Presentacion.Controllers.Shared;
 using MVC.Presentacion.Models.Catalogos;
+using MVC.Presentacion.Models.Seguridad;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,39 +15,35 @@ namespace MVC.Presentacion.Controllers
     public class CentroCostoController : MainController
     {
         string tkn = string.Empty;
-        public ActionResult CentroCosto()
+        public ActionResult CentroCosto(string mjs = null)
         {
-            if (Session["StringToken"] != null)
-            {
-                tkn = Session["StringToken"].ToString();
-                ViewBag.TiposCentrosCosto = CatalogoServicio.BuscarTipoCentrosCosto(tkn);
-                ViewBag.EstacionesCarburacion = CatalogoServicio.GetListaEstacionCarburacion(tkn);
-                ViewBag.UnidadAlmacenGas = CatalogoServicio.GetListaUnidadAlmcenGas(TokenServicio.ObtenerIdEmpresa(tkn), tkn);
-                ViewBag.EquipoTransporte = CatalogoServicio.GetListaEquiposTransporte(tkn);
-                ViewBag.Empresas = CatalogoServicio.Empresas(tkn);
-                return View(CatalogoServicio.InitCentroCosto(tkn));
-            }
-            else
-                return View(AutenticacionServicio.InitIndex(new Models.Seguridad.LoginModel()));
+            if (Session["StringToken"] == null) return RedirectToAction("Index", "Home");
+            tkn = Session["StringToken"].ToString();
+            if (mjs != null) ViewBag.Mjs = mjs;
+            ViewBag.TiposCentrosCosto = CatalogoServicio.BuscarTipoCentrosCosto(tkn);
+            ViewBag.EstacionesCarburacion = CatalogoServicio.GetListaEstacionCarburacion(tkn);
+            ViewBag.UnidadAlmacenGas = CatalogoServicio.GetListaUnidadAlmcenGas(TokenServicio.ObtenerIdEmpresa(tkn), tkn);
+            ViewBag.EquipoTransporte = CatalogoServicio.GetListaEquiposTransporte(tkn);
+            ViewBag.Empresas = CatalogoServicio.Empresas(tkn);
+            if (ViewData["RespuestaDTO"] != null)
+                Validar((RespuestaDTO)ViewData["RespuestaDTO"]);
+            return View(CatalogoServicio.InitCentroCosto(tkn));
+
         }
         public ActionResult Crear(CentroCostoModel model)
         {
-            if (Session["StringToken"] != null)
+            if (Session["StringToken"] == null) return RedirectToAction("Index", "Home");
+            tkn = Session["StringToken"].ToString();
+            var respuesta = CatalogoServicio.CrearCentroCosto(model, tkn);
+            if (respuesta.Exito)
             {
-                tkn = Session["StringToken"].ToString();
-                var respuesta = CatalogoServicio.CrearCentroCosto(model, tkn);
-                if (respuesta.Exito)
-                {
-                    return CentroCosto();
-                }
-                else
-                {
-                    ViewBag.MensajeError = respuesta.Mensaje;
-                    return View("CentroCosto", CatalogoServicio.InitCentroCosto(tkn));
-                }
+                return RedirectToAction("CentroCosto", "Proceso exitoso");
             }
             else
-                return View(AutenticacionServicio.InitIndex(new Models.Seguridad.LoginModel()));
+            {
+                ViewData["RespuestaDTO"] = respuesta;
+                return RedirectToAction("CentroCosto", CatalogoServicio.InitCentroCosto(tkn));
+            }
         }
         public ActionResult ActivarEditar(int? id, CentroCostoModel model)
         {
@@ -101,6 +98,22 @@ namespace MVC.Presentacion.Controllers
             }
             else
                 return View(AutenticacionServicio.InitIndex(new Models.Seguridad.LoginModel()));
+        }
+        private string Validar(RespuestaDTO Resp = null)
+        {
+            string Mensaje = string.Empty;
+            ModelState.Clear();
+            if (Resp != null)
+            {
+                if (Resp.ModelStatesStandar != null)
+                    foreach (var error in Resp.ModelStatesStandar.ToList())
+                    {
+                        ModelState.AddModelError(error.Key, error.Value);
+                    }
+                if (Resp.MensajesError != null)
+                    Mensaje = Resp.MensajesError[0];
+            }
+            return Mensaje;
         }
     }
 }
--- 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Controllers/OrdenCompraController.cs
+++ 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Controllers/OrdenCompraController.cs
@@ -36,6 +36,7 @@ namespace MVC.Presentacion.Controllers
             ViewBag.IVAs = CatalogoServicio.ListaIVA();
             ViewBag.IEPs = CatalogoServicio.ListaIEPS();
             ViewBag.Estatus = model.IdOrdenCompraEstatus;
+            if (TempData["RespuestaDTO"] != null) ViewBag.MensajeError = Validar((RespuestaDTO)TempData["RespuestaDTO"]);
             return View(model);
         }
         public ActionResult CrearOrdenCompra(OrdenCompraModel model)
@@ -118,6 +119,7 @@ namespace MVC.Presentacion.Controllers
             tkn = Session["StringToken"].ToString();
             var complemeto = OrdenCompraServicio.InitComplemento(id, tkn);
             ViewBag.Proveedor = complemeto.Proveedor;
+            if (TempData["RespuestaDTO"] != null) ViewBag.MensajeError = Validar((RespuestaDTO)TempData["RespuestaDTO"]);
             return View(complemeto);
         }
         [HttpPost]
@@ -198,7 +200,7 @@ namespace MVC.Presentacion.Controllers
             tkn = Session["StringToken"].ToString();
             var ocp = OrdenCompraServicio.InitOrdenCompraPago(id, tkn);
             ViewBag.FormasPago = CatalogoServicio.ListaFormaPago(tkn);
-            if (TempData["RespuestaDTO"] != null) ViewBag.MensajeError = Validar((RespuestaDTO)TempData["RespuestaDTO"]);        
+            if (TempData["RespuestaDTO"] != null) ViewBag.MensajeError = Validar((RespuestaDTO)TempData["RespuestaDTO"]);
             return View(ocp);
         }
         public ActionResult ConfirmarPago(OrdenCompraPagoDTO dto = null)
@@ -224,10 +226,7 @@ namespace MVC.Presentacion.Controllers
             ViewBag.IEPs = CatalogoServicio.ListaIEPS();
             ViewBag.CuentasContables = CatalogoServicio.ListaCtaCtble(tkn).Select(cc => new SelectListItem { Value = cc.IdCuentaContable.ToString(), Text = cc.Descripcion }).ToList();
             ViewBag.Proveedores = CatalogoServicio.ListaProveedores(tkn).Select(p => new SelectListItem { Value = p.IdProveedor.ToString(), Text = p.NombreComercial }).ToList();
-
-            RespuestaDTO Resp = new RespuestaDTO();
-            if (TempData["RespuestaDTO"] != null) Resp = (RespuestaDTO)TempData["RespuestaDTO"];
-            ViewBag.MensajeError = Validar(Resp);
+            if (TempData["RespuestaDTO"] != null) ViewBag.MensajeError = Validar((RespuestaDTO)TempData["RespuestaDTO"]);            
             return View(complemeto);          
         }
         private string Validar(RespuestaDTO Resp = null)
