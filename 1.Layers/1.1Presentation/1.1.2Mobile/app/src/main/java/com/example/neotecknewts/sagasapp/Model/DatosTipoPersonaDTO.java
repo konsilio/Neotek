45f6c8c1e532bee7c2dd5e0d9ed19206be7a073d
Jorge Omar Tovar Martínez <jorge.tovar@neoteck.com.mx>
2018-10-12 18:18:00 -0500

    JOTM-12/10/2018-Mobile- interfaz y catalogo tipo de persona
    * Ajusta responsive  de tablet a teléfono
    * Agrega consumo de servicio tipo de persona en ventas

--- 1.Layers/1.1Presentation/1.1.2Mobile/app/src/main/java/com/example/neotecknewts/sagasapp/Model/DatosTipoPersonaDTO.java
+++ 1.Layers/1.1Presentation/1.1.2Mobile/app/src/main/java/com/example/neotecknewts/sagasapp/Model/DatosTipoPersonaDTO.java
@@ -1,5 +1,23 @@
 package com.example.neotecknewts.sagasapp.Model;
 
-public class DatosTipoPersonaDTO extends RespuestaDTO {
+import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatosTipoPersonaDTO extends RespuestaDTO {
+    @SerializedName("tipoPersona")
+    private List<TipoPersonaDTO> tipoPersona;
+
+    public DatosTipoPersonaDTO(){
+        this.tipoPersona = new ArrayList<>();
+    }
+
+    public List<TipoPersonaDTO> getTipoPersona() {
+        return tipoPersona;
+    }
+
+    public void setTipoPersona(List<TipoPersonaDTO> tipoPersona) {
+        this.tipoPersona = tipoPersona;
+    }
 }
