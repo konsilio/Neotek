9d601f473c147c412f7c15855435f279ee832d82
Jaime Santillan <jaime.santillan@neoteck.com.mx>
2018-10-29 17:59:58 -0600

    JSA-29/10/2018 Nueva orde de compra
    Se adapta nueva orden de compra (Producto) con tabla DevExpress

--- 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Models/OrdenCompra/ProductoOCDTO.cs
+++ 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Models/OrdenCompra/ProductoOCDTO.cs
@@ -1,4 +1,6 @@
-﻿using Exceptions.MainModule.Validaciones;
+﻿using DevExpress.Web.Demos;
+using Exceptions.MainModule.Validaciones;
+using MVC.Presentacion.App_Code;
 using MVC.Presentacion.Models.Requisicion;
 using System;
 using System.Collections.Generic;
@@ -11,5 +13,19 @@ namespace MVC.Presentacion.Models.OrdenCompra
     [Serializable]
     public class ProductoOCDTO : OrdenCompraProductoDTO
     {       
+        public static List<ProductoOCDTO> GetEditableProducts()
+        {
+            List<ProductoOCDTO> products = (List<ProductoOCDTO>)HttpContext.Current.Session["Products"];
+            if (products == null)
+            {
+                //products = OrdenCompraServicio.DatosRequisicion(1195, HttpContext.Current.Session["StringToken"].ToString()).ProductosOC.ToList();
+                HttpContext.Current.Session["Products"] = products;
+            }
+            return products;
+        }
+        public static ProductoOCDTO GetEditableProduct(int productID)
+        {
+            return (from product in GetEditableProducts() where product.IdProducto == productID select product).FirstOrDefault();
+        }
     }
 }
