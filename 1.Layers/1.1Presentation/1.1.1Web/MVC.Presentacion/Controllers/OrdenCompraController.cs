8bd511e16cfe392f90a3fd37689776d5ad75b61e
Jaime Santillan <jaime.santillan@neoteck.com.mx>
2018-11-06 11:42:13 -0600

    JSA-06/11/2018 Avance en ajustes
    Avance en ajustes Orden Compra

--- 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Controllers/OrdenCompraController.cs
+++ 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Controllers/OrdenCompraController.cs
@@ -40,12 +40,16 @@ namespace MVC.Presentacion.Controllers
             if (Session["StringToken"] == null) return RedirectToAction("Index", "Home");
             tkn = Session["StringToken"].ToString();
             int idOc = id ?? 0;
-            var model = OrdenCompraServicio.BuscarOrdenCompra(idOc, tkn);
+            var model = OrdenCompraServicio.BuscarOrdenCompra(idOc, tkn);            
             ViewBag.CuentasContables = CatalogoServicio.ListaCtaCtble(tkn).Select(cc => new SelectListItem { Value = cc.IdCuentaContable.ToString(), Text = cc.Descripcion }).ToList();
-            ViewBag.Proveedores = CatalogoServicio.ListaProveedores(tkn).Select(p => new SelectListItem { Value = p.IdProveedor.ToString(), Text = p.NombreComercial }).ToList();
+            //ViewBag.Proveedores = CatalogoServicio.ListaProveedores(tkn).Select(p => new SelectListItem { Value = p.IdProveedor.ToString(), Text = p.NombreComercial }).ToList();
             ViewBag.IVAs = CatalogoServicio.ListaIVA();
             ViewBag.IEPs = CatalogoServicio.ListaIEPS();
             ViewBag.Estatus = model.IdOrdenCompraEstatus;
+            if (model.EsGas || model.EsTransporteGas)
+                ViewBag.EsGasTransporte = true;
+            else
+                ViewBag.EsGasTransporte = false;
             if (TempData["RespuestaDTO"] != null) ViewBag.MensajeError = Validar((RespuestaDTO)TempData["RespuestaDTO"]);
             return View(model);
         }
