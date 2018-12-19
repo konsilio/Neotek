44bb8545a4652d9b7d6cf6485db6efac5087e4cd
Jaime Santillan <jaime.santillan@neoteck.com.mx>
2018-12-18 18:30:41 -0600

    JSA - Ingreso gasto avance

---
+++ 1.Layers/1.3Application/1.3.2MainModule/Application.MainModule/Servicios/AccesoADatos/EgresoDataAccess.cs
@@ -0,0 +1,98 @@
+﻿using Application.MainModule.DTOs.Respuesta;
+using Application.MainModule.UnitOfWork;
+using Exceptions.MainModule;
+using Exceptions.MainModule.Validaciones;
+using Sagas.MainModule.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.MainModule.Servicios.AccesoADatos
+{
+    public class EgresoDataAccess
+    {
+        private SagasDataUow uow;
+        public EgresoDataAccess()
+        {
+            uow = new SagasDataUow();
+        }
+        public RespuestaDto Insertar(Egreso entidad)
+        {
+            RespuestaDto _respuesta = new RespuestaDto();
+            using (uow)
+            {
+                try
+                {
+                    uow.Repository<Egreso>().Insert(entidad);
+                    uow.SaveChanges();
+                    _respuesta.Id = entidad.IdEgreso;
+                    _respuesta.EsInsercion = true;
+                    _respuesta.Exito = true;
+                    _respuesta.ModeloValido = true;
+                    _respuesta.Mensaje = Exito.OK;
+                }
+                catch (Exception ex)
+                {
+                    _respuesta.Exito = false;
+                    _respuesta.Mensaje = string.Format(Error.C0002, "del Egreso");
+                    _respuesta.MensajesError = CatchInnerException.Obtener(ex);
+                }
+            }
+            return _respuesta;
+        }
+        public RespuestaDto Actualizar(Egreso entidad)
+        {
+            RespuestaDto _respuesta = new RespuestaDto();
+            using (uow)
+            {
+                try
+                {
+                    uow.Repository<Egreso>().Update(entidad);
+                    uow.SaveChanges();
+                    _respuesta.Id = entidad.IdEgreso;
+                    _respuesta.Exito = true;
+                    _respuesta.EsActulizacion = true;
+                    _respuesta.ModeloValido = true;
+                    _respuesta.Mensaje = Exito.OK;
+                }
+                catch (Exception ex)
+                {
+                    _respuesta.Exito = false;
+                    _respuesta.Mensaje = string.Format(Error.C0003, "del Egreso"); ;
+                    _respuesta.MensajesError = CatchInnerException.Obtener(ex);
+                }
+            }
+            return _respuesta;
+        }
+        public List<Egreso> BuscarTodos()
+        {
+            return uow.Repository<Egreso>().GetAll().ToList();
+        }
+        public List<Egreso> BuscarTodos(short id)
+        {
+            return uow.Repository<Egreso>().Get(x => x.IdEmpresa.Equals(id)).ToList();
+        }
+        public List<Egreso> BuscarPorCentroCosto(int id)
+        {
+            return uow.Repository<Egreso>().Get(x => x.IdCentroCosto.Equals(id)).ToList();
+        }
+        public List<Egreso> BuscarPoCuentaContable(int id)
+        {
+            return uow.Repository<Egreso>().Get(x => x.IdCuentaContable.Equals(id)).ToList();
+        }
+        public Egreso BuscarPorTipoEgreso(short id)
+        {
+            return uow.Repository<Egreso>().GetSingle(x => x.IdTipoEgreso.Equals(id));
+        }
+        public Egreso BuscarPorExterno(bool esExterno)
+        {
+            return uow.Repository<Egreso>().GetSingle(x => x.EsExterno.Equals(esExterno));
+        }
+        public List<Egreso> BuscarTodos(DateTime fi, DateTime ff)
+        {
+            return uow.Repository<Egreso>().Get(x => x.FechaRegistro > fi && x.FechaRegistro < ff ).ToList();
+        }
+    }
+}
---
+++ 1.Layers/1.3Application/1.3.2MainModule/Application.MainModule/Servicios/AccesoADatos/IngresoDataAccess.cs
@@ -0,0 +1,94 @@
+﻿using Application.MainModule.DTOs.Respuesta;
+using Application.MainModule.UnitOfWork;
+using Exceptions.MainModule;
+using Exceptions.MainModule.Validaciones;
+using Sagas.MainModule.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.MainModule.Servicios.AccesoADatos
+{
+    public class IngresoDataAccess
+    {
+        private SagasDataUow uow;
+        public IngresoDataAccess()
+        {
+            uow = new SagasDataUow();
+        }
+        public RespuestaDto Insertar(Ingreso entidad)
+        {
+            RespuestaDto _respuesta = new RespuestaDto();
+            using (uow)
+            {
+                try
+                {
+                    uow.Repository<Ingreso>().Insert(entidad);
+                    uow.SaveChanges();
+                    _respuesta.Id = entidad.IdIngreso;
+                    _respuesta.EsInsercion = true;
+                    _respuesta.Exito = true;
+                    _respuesta.ModeloValido = true;
+                    _respuesta.Mensaje = Exito.OK;
+                }
+                catch (Exception ex)
+                {
+                    _respuesta.Exito = false;
+                    _respuesta.Mensaje = string.Format(Error.C0002, "del ingreso");
+                    _respuesta.MensajesError = CatchInnerException.Obtener(ex);
+                }
+            }
+            return _respuesta;
+        }
+        public RespuestaDto Actualizar(Ingreso entidad)
+        {
+            RespuestaDto _respuesta = new RespuestaDto();
+            using (uow)
+            {
+                try
+                {
+                    uow.Repository<Ingreso>().Update(entidad);
+                    uow.SaveChanges();
+                    _respuesta.Id = entidad.IdIngreso;
+                    _respuesta.Exito = true;
+                    _respuesta.EsActulizacion = true;
+                    _respuesta.ModeloValido = true;
+                    _respuesta.Mensaje = Exito.OK;
+                }
+                catch (Exception ex)
+                {
+                    _respuesta.Exito = false;
+                    _respuesta.Mensaje = string.Format(Error.C0003, "del ingreso"); ;
+                    _respuesta.MensajesError = CatchInnerException.Obtener(ex);
+                }
+            }
+            return _respuesta;
+        }
+        public List<Ingreso> BuscarTodos()
+        {
+            return uow.Repository<Ingreso>().GetAll().ToList();
+        }
+        public List<Ingreso> BuscarTodos(short id)
+        {
+            return uow.Repository<Ingreso>().Get(x => x.IdEmpresa.Equals(id)).ToList();
+        }
+        public List<Ingreso> BuscarTodos(DateTime fi, DateTime ff)
+        {
+            return uow.Repository<Ingreso>().Get(x => x.FechaRegistro > fi && x.FechaRegistro < ff).ToList();
+        }
+        public List<Ingreso> BuscarPorCentroCosto(int id)
+        {
+            return uow.Repository<Ingreso>().Get(x => x.IdCentroCosto.Equals(id)).ToList();
+        }
+        public List<Ingreso> BuscarPoCuentaContable(int id)
+        {
+            return uow.Repository<Ingreso>().Get(x => x.IdCuentaContable.Equals(id)).ToList();
+        }
+        public Ingreso Buscar(string Ticket)
+        {
+            return uow.Repository<Ingreso>().GetSingle(x => x.Ticket.Equals(Ticket));
+        }
+    }
+}
