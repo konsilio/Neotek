0822a87fff3948eb2edb1d11b940f07a2b906710
glcjal <celia.ambriz@neoteck.com.mx>
2018-12-27 17:54:17 -0600

    CJAL-27/12/2018-Finalizacion desarrollo Pedidos

---
+++ 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Models/Cobranza/CargosModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Presentacion.Models.Cobranza
+{
+    public class CargosModel
+    {
+        public int IdCargo { get; set; }
+        public int IdCliente { get; set; }
+        public short IdEmpresa { get; set; }
+        public string Ticket { get; set; }
+        public System.DateTime FechaRegistro { get; set; }
+        public decimal TotalCargo { get; set; }
+        public decimal TotalAbonos { get; set; }
+        public bool VentaExtraordinaria { get; set; }
+        public bool Activo { get; set; }
+        public System.DateTime FechaVencimiento { get; set; }
+        public bool Saldada { get; set; }
+    }
+}
