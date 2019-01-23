6b8b4e9cb7d65298106144618fc304a40e722731
glcjal <celia.ambriz@neoteck.com.mx>
2019-01-23 15:58:39 -0600

    Auto stash before merge of "EquipoTransporte" and "base"
    cjal-23/01/2019 avance  parque vehicular

---
+++ 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Controllers/EquipoTransporteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Presentacion.Controllers
+{
+    public class EquipoTransporteController : Controller
+    {
+        string _tkn = string.Empty;
+        // GET: EquipoTransporte
+        public ActionResult Index(string placa=null,string vehiculo = null, string msj = null)
+        {
+            return View();
+        }
+    }
+}
