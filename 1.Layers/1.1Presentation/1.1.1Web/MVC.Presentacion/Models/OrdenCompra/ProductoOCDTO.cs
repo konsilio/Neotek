e31bb36dbdfdbd7fc9000ee1946d79c210fc8acc
Jaime Santillan <jaime.santillan@neoteck.com.mx>
2019-01-23 13:11:24 -0600

    JSA-Creacion de ventana de asignacion

--- 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Models/OrdenCompra/ProductoOCDTO.cs
+++ 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Models/OrdenCompra/ProductoOCDTO.cs
@@ -1,12 +1,4 @@
-﻿using DevExpress.Web.Demos;
-using Exceptions.MainModule.Validaciones;
-using MVC.Presentacion.App_Code;
-using MVC.Presentacion.Models.Requisicion;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿using System;
 
 namespace MVC.Presentacion.Models.OrdenCompra
 {
