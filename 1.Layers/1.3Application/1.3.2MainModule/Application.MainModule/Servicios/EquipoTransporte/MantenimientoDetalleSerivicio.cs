3378c19ccdea2b34fe1b4c05b148db77468b1499
Jaime Santillan <jaime.santillan@neoteck.com.mx>
2019-01-25 10:27:33 -0600

    JSA - Avance estructura nuevos modulos
    Modulos afectados: 
    Mantenimiento
    Recarga combustible

---
+++ 1.Layers/1.3Application/1.3.2MainModule/Application.MainModule/Servicios/EquipoTransporte/MantenimientoDetalleSerivicio.cs
@@ -0,0 +1,31 @@
+﻿using Application.MainModule.DTOs.Respuesta;
+using Application.MainModule.Servicios.AccesoADatos;
+using Sagas.MainModule.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.MainModule.Servicios.EquipoTransporte
+{
+    public static class MantenimientoDetalleSerivicio
+    {
+        public static RespuestaDto Crear(DetalleMantenimiento entidad)
+        {
+            return new MantenimientoDetDataAccess().Insertar(entidad);
+        }
+        public static RespuestaDto Actuualizar(DetalleMantenimiento entidad)
+        {
+            return new MantenimientoDetDataAccess().Actualizar(entidad);
+        }
+        public static List<DetalleMantenimiento> Buscar()
+        {
+            return new MantenimientoDetDataAccess().Obtener();
+        }
+        public static DetalleMantenimiento Buscar(int id)
+        {
+            return new MantenimientoDetDataAccess().Obtener(id);
+        }
+    }
+}
--- 1.Layers/1.3Application/1.3.2MainModule/Application.MainModule/Servicios/EquipoTransporte/RecargaCombustibleServicio.cs
+++ 1.Layers/1.3Application/1.3.2MainModule/Application.MainModule/Servicios/EquipoTransporte/RecargaCombustibleServicio.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Application.MainModule.DTOs.Respuesta;
+using Application.MainModule.Servicios.AccesoADatos;
+using Sagas.MainModule.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,25 @@ namespace Application.MainModule.Servicios.EquipoTransporte
 {
     public static class RecargaCombustibleServicio
     {
-
+        public static RespuestaDto Crear(DetalleRecargaCombustible entidad)
+        {
+            return new RecargaCombustibleDataAccess().Insertar(entidad);
+        }
+        public static RespuestaDto Actuualizar(DetalleRecargaCombustible entidad)
+        {
+            return new RecargaCombustibleDataAccess().Actualizar(entidad);
+        }
+        public static List<DetalleRecargaCombustible> Buscar()
+        {
+            return new RecargaCombustibleDataAccess().Obtener();
+        }
+        public static DetalleRecargaCombustible Buscar(int id)
+        {
+            return new RecargaCombustibleDataAccess().Obtener(id);
+        }
+        //public static List<DetalleRecargaCombustible> Buscar(short id)
+        //{Buscar por Empresa
+        //    return new RecargaCombustibleDataAccess().Obtener().Select(x => x.CCamioneta.);
+        //}
     }
 }
