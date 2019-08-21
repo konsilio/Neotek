5ced708f66577b8d3c63bc550d5a66077b9c4631
jimmy <jaime.santillan@neoteck.com.mx>
2019-08-21 18:34:55 -0500

    MC - Correcciones Requisicion

--- 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Models/Requisicion/RequisicionDTO.cs
+++ 1.Layers/1.1Presentation/1.1.1Web/MVC.Presentacion/Models/Requisicion/RequisicionDTO.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Exceptions.MainModule.Validaciones;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVC.Presentacion.Models.Requisicion
 {
-    [Serializable]
+    
     public class RequisicionDTO
     {       
         public int IdRequisicion { get; set; }       
@@ -38,4 +40,55 @@ namespace MVC.Presentacion.Models.Requisicion
         public bool EsExterno { get; set; }
 
     }
+
+    public class RequisicionInputDTO
+    {
+
+        public int IdRequisicion { get; set; }
+        public int IdUsuarioSolicitante { get; set; }
+        public int IdUsuarioAutorizacion { get; set; }
+        public int IdUsuarioRevision { get; set; }
+        public string UsuarioSolicitante { get; set; }
+        public short IdEmpresa { get; set; }
+        public string NombreComercial { get; set; }
+        public string NumeroRequisicion { get; set; }
+        [Required(ErrorMessage = Error.R0002)]
+       
+        public string MotivoRequisicion { get; set; }
+        [Required(ErrorMessage = Error.R0002)]
+       
+        public string RequeridoEn { get; set; }
+        public string OpinionAlmacen { get; set; }
+        public string MotivoCancelacion { get; set; }
+        public byte IdRequisicionEstatus { get; set; }
+        public string RequisicionEstatus { get; set; }
+        [Required(ErrorMessage = Error.R0002)]
+        
+        public DateTime FechaRequerida { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public DateTime FechaRevision { get; set; }
+        public DateTime FechaAutorizacion { get; set; }
+        public List<RequisicionProductoDTO> Productos { get; set; }
+        [Required(ErrorMessage = Error.R0002)]
+       
+        public int IdProducto { get; set; }
+        public string Producto { get; set; }
+        [Required(ErrorMessage = Error.R0002)]
+       
+        public int IdTipoProducto { get; set; }
+        public string TipoProducto { get; set; }
+        [Required(ErrorMessage = Error.R0002)]
+        
+        public int IdCentroCosto { get; set; }
+        public string CentroCosto { get; set; }
+        public int IdUnidad { get; set; }
+        public string Unidad { get; set; }
+        public decimal Cantidad { get; set; }
+        public string Aplicacion { get; set; }
+        public bool EsExterno { get; set; }
+
+
+    }
+
+
 }
